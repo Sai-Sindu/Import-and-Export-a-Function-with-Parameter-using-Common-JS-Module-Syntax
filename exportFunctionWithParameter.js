8e553eddf1e4b75041bef902488cc4a589015ce8
Sai Sindu <saisindu9398640181@gmail.com>
2023-08-14 11:30:19 +0000

    Import and Export a Function with Parameter using Common JS Module Syntax

---
+++ exportFunctionWithParameter.js
@@ -0,0 +1,4 @@
+const square = function (num) {
+  return num * num;
+};
+module.exports = square;
---
+++ importFunctionWithParameter.js
@@ -0,0 +1,2 @@
+const square = require("./exportFunctionWithParameter.js");
+console.log(square(2));
